5aef7ce8c0e6cc00190bfef83401af26a88f9c2e
hashimoto <m.hashimoto@risewill.co.jp>
2021-10-22 10:59:06 +0900

    [mod]記述整理、hamburger active時のクラス名変更

--- js/app.js
+++ js/app.js
@@ -1,22 +1,21 @@
 $(function(){
-    $('.hamburger-btn').on('click', function() {
-      $('body').toggleClass('active');
-      return false;
-    });
+  //hamburger menu
+  $('.hamburger-btn').on('click', function() {
+    $('body').toggleClass('is-active');
+    return false;
   });
 
-  $(function(){//smooth scroll
-    $('a[href^="#"]').click(function(){
-      var speed = 500;
-      var href= $(this).attr("href");
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top;
-      $("html, body").animate({scrollTop:position}, speed, "swing");
-      return false;
-    });
+  //smooth scroll
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
   });
 
-$(function () {//page top
+  //page top
   var pagetop = $('.top-btn');
   pagetop.hide();
   $(window).scroll(function () {
@@ -32,4 +31,5 @@ $(function () {//page top
     }, 500);
     return false;
   });
+
 });
